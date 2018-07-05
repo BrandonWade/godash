9bbaeab1a73227b20973416e2a584f44dbc44f17
Brandon Wade <brandon_wade2005@hotmail.com>
2018-07-04 23:29:23 -0500

    Add Includes and DifferenceSubstr functions.

--- godash.go
+++ godash.go
@@ -1,5 +1,7 @@
 package godash
 
+import "strings"
+
 // Difference - returns a slice containing all elements in src that are not in dst
 func Difference(src, dst []interface{}) []interface{} {
 	var diff []interface{}
@@ -12,6 +14,28 @@ func Difference(src, dst []interface{}) []interface{} {
 	}
 
 	return diff
+}
+
+// DifferenceSubstr - return a slice containing all elements in keys where any element of val is not a substring
+func DifferenceSubstr(keys, vals []string) []string {
+	items := []string{}
+
+	for _, key := range keys {
+		for _, val := range vals {
+			if key != val && !strings.Contains(key, val) {
+				items = append(items, key)
+			}
+		}
+	}
+
+	return items
+}
+
+// Includes - returns a bool indicating if a key is in a slice
+func Includes(key interface{}, vals []interface{}) bool {
+	valsMap := ToMap(vals)
+
+	return valsMap[key] != nil
 }
 
 // ToMap - convert a slice to a map for fast lookups
