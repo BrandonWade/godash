98c826452e49aa1638977f40dfeea240904cc9e9
Brandon Wade <brandon_wade2005@hotmail.com>
2018-07-05 19:09:37 -0500

    Replace generic implementations with string equivalents.

--- godash.go
+++ godash.go
@@ -2,12 +2,12 @@ package godash
 
 import "strings"
 
-// Difference - returns a slice containing all elements in src that are not in dst
-func Difference(src, dst []interface{}) []interface{} {
-	var diff []interface{}
-	dstMap := ToMap(dst)
+// DifferenceStr - returns a slice of elements in items that are not in vals
+func DifferenceStr(items, vals []string) []string {
+	diff := []string{}
+	dstMap := ToMap(diff)
 
-	for _, item := range src {
+	for _, item := range items {
 		if _, ok := dstMap[item]; !ok {
 			diff = append(diff, item)
 		}
@@ -16,34 +16,36 @@ func Difference(src, dst []interface{}) []interface{} {
 	return diff
 }
 
-// DifferenceSubstr - return a slice containing all elements in keys where any element of val is not a substring
-func DifferenceSubstr(keys, vals []string) []string {
-	items := []string{}
+// DifferenceSubstr - return a slice of elements in items excluding elements where any element from vals is a substring
+func DifferenceSubstr(items, vals []string) []string {
+	diff := []string{}
 
-	for _, key := range keys {
-		for _, val := range vals {
-			if key != val && !strings.Contains(key, val) {
-				items = append(items, key)
-			}
+	for _, item := range items {
+		if !IncludesSubstr(vals, item) {
+			diff = append(diff, item)
 		}
 	}
 
-	return items
+	return diff
 }
 
-// Includes - returns a bool indicating if a key is in a slice
-func Includes(key interface{}, vals []interface{}) bool {
-	valsMap := ToMap(vals)
+// IncludesSubstr - returns a boolean indicating if items contains key
+func IncludesSubstr(items []string, key string) bool {
+	for _, item := range items {
+		if key == item || strings.Contains(key, item) {
+			return true
+		}
+	}
 
-	return valsMap[key] != nil
+	return false
 }
 
-// ToMap - convert a slice to a map for fast lookups
-func ToMap(items []interface{}) map[interface{}]interface{} {
-	itemMap := make(map[interface{}]interface{})
+// ToMap - convert a slice of strings to a map for fast lookups
+func ToMap(items []string) map[string]string {
+	itemMap := make(map[string]string)
 
 	for _, item := range items {
-		itemMap[item] = nil
+		itemMap[item] = ""
 	}
 
 	return itemMap
